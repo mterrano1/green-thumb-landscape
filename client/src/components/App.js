40c7898cd0a19e37b134512fc490a17b1383c516
Christopher Terrano <mterrano1@yahoo.com>
2022-06-25 14:27:47 -0500

    create plant id route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MyProvider } from './MyContext'
 import Plants from './Plants';
+import Plant from './Plant';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         {/* <Route exact path='/' element={<Home />} /> */}
         <Route exact path='/plants' element={<Plants />} />
+        <Route exact path='/plants/:id' element={<Plant />} />
       </Routes>
     </MyProvider>
   );
