36d75a852dcd1cc5d5661fe85057ba50c24505fa
Christopher Terrano <mterrano1@yahoo.com>
2022-06-27 14:40:04 -0500

    create home page

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <hr/>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
