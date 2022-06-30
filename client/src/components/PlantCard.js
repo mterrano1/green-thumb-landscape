c422e90951eb70a8e2487fb92e467bba30efb18d
Christopher Terrano <mterrano1@yahoo.com>
2022-06-30 12:42:04 -0500

    create PlantCard component

--- client/src/components/PlantCard.js
+++ client/src/components/PlantCard.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-const PlantCard = ( {plant} ) => {
+const PlantCard = ({ plant }) => {
     const {id, name, image} = plant
 
+
     return (
-        <li>
+
+        <li className='card'>
             <Link to={`/plants/${id}`}>
                 <h4>{name}</h4>
                 <img src={image} alt={name} />
-                <p>{id}</p>
             </Link>
         </li>
     )
