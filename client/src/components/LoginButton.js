0257d2446c0b01286424503daf1a5cc60c3fedcc
Christopher Terrano <mterrano1@yahoo.com>
2022-06-30 12:40:10 -0500

    create LoginButton

---
+++ client/src/components/LoginButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginButton = () => {
+
+    return (
+        <Link to={'/login'}>
+            <button>Login</button>
+        </Link>
+    );
+}
+
+export default LoginButton;
