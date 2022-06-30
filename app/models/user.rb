9f9ebc2c6929a3c18363d7697360c2759892e971
Christopher Terrano <mterrano1@yahoo.com>
2022-06-30 13:38:07 -0500

    create user associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
+    has_many :reviews
+    has_many :plants, through: :reviews
+
     validates :username, presence: true, uniqueness: true
 end
