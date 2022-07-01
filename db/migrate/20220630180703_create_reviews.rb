dc4b3af2e21bee164e5ed273f73dc98b8d6b5f64
Christopher Terrano <mterrano1@yahoo.com>
2022-07-01 15:13:30 -0500

    changed value of likes from boolean to integer

--- db/migrate/20220630180703_create_reviews.rb
+++ db/migrate/20220630180703_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.string :comment
-      t.boolean :like
+      t.integer :like
       t.integer :plant_id
       t.integer :user_id
 
