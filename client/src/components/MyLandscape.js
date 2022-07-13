65e18be1648a06d033a5a4673b9e761187a34496
Christopher Terrano <mterrano1@yahoo.com>
2022-07-13 13:26:06 -0500

    create map to display user plants

--- client/src/components/MyLandscape.js
+++ client/src/components/MyLandscape.js
@@ -1,15 +1,28 @@
 import React, { useContext } from 'react'
 import { UserContext } from './UserContext';
+import { Link } from 'react-router-dom';
 
 const MyLandscape = () => {
-    const {user, loggedIn} = useContext(UserContext);
+    const {user, loggedIn, userPlants} = useContext(UserContext);
+
+    const landscapePlants = userPlants.map(plant => (
+        <li key={plant.id} className='card'>
+            <Link to={`/plants/${plant.id}`}>
+                <h3>{plant.name}</h3>
+                <img src={plant.image} alt={plant.name}/>
+                <h4></h4>
+            </Link>
+        </li>
+    ))
 
     if (loggedIn) {
         return (
             <div>
                 <hr/>
-                <h2 class='capitalize'>{user.username}'s garden</h2>
-                <h2 class='capitalize'>{user.username}'s reviews</h2>
+                <h1 className='capitalize'>{user.username}'s Landscape</h1>
+                <ul className='cards'>
+                    {landscapePlants}
+                </ul>
             </div>
         );
     } else {
