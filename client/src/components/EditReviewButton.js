7b63af8e5aea33259bb61f6d68ea8e54ca204f6e
Christopher Terrano <mterrano1@yahoo.com>
2022-07-13 13:22:22 -0500

    add link to edit component

--- client/src/components/EditReviewButton.js
+++ client/src/components/EditReviewButton.js
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react'
-import { UserContext } from './UserContext';
+import React from "react";
 import { Link } from 'react-router-dom';
 
-const EditReviewButton = ({ plantId }) => {
-    const {user, loggedIn} = useContext(UserContext);
+
+const EditReviewButton = ({ plantId, userReview }) => {
+
 
     return (
+        <Link to={`/plants/${plantId}/reviews/${userReview.id}`}>
             <button className={'button'}>Edit review</button>
+        </Link>
     );
 }
 
