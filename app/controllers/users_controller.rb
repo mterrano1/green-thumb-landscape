090886a7e9f6003652ac1321e23669b6271c6922
Christopher Terrano <mterrano1@yahoo.com>
2022-07-15 11:05:31 -0500

    trim code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-    # before_action :authorize, only: :show
-    # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     skip_before_action :authorize, only: :create
 
     #POST /signup
@@ -12,12 +10,6 @@ class UsersController < ApplicationController
 
     #GET /me
     def show
-        # user = User.find_by(id: session[:user_id])
-        # if user
-        #     render json: user
-        # else
-        #     render json: { errors: ["Not authorized"] }, status: :unauthorized
-        # end
         render json: @current_user
     end
 
@@ -26,13 +18,5 @@ class UsersController < ApplicationController
     def user_params
         params.permit(:username, :password, :password_confirmation)
     end
-
-    # def authorize
-    #     return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
-    # end
-
-    # def render_unprocessable_entity_response(e)
-    #     render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity 
-    # end
 
 end
